2e56e5ac8efabce18214caf7e47b06882f3cb65e
wingding12 <144286538+wingding12@users.noreply.github.com>
2025-02-17 03:34:21 -0500

    temp

--- js/style.js
+++ js/style.js
@@ -4,32 +4,22 @@ import { IS_ELECTRON } from "./electron.js";
 import { IS_PUTER } from "./puter.js";
 
 const style = {
-    SUPPORTED_STYLES: ["default", "minimal", "standalone", "electron", "puter"],
+    SUPPORTED_STYLES: ["default", "minimal", "standalone", "electron"],
     DEFAULT_STYLE: "default",
     apply(name) {
         const resolvedName = style.SUPPORTED_STYLES.includes(name) ? name : style.DEFAULT_STYLE;
         if (resolvedName !== "default") {
             style.apply("default");
             document.querySelectorAll(`.judge0-${resolvedName}-hidden`).forEach(e => {
-                e.classList.add("judge0-style-hidden");
+                e.classList.add("judge0-hidden");
             });
         } else {
             style.SUPPORTED_STYLES.forEach(s => style.reverse(s));
         }
-
-        style.SUPPORTED_STYLES.forEach(s => {
-            document.querySelectorAll(`.judge0-${s}-visible`).forEach(e => {
-                if (s === resolvedName) {
-                    e.classList.remove("judge0-style-hidden");
-                } else {
-                    e.classList.add("judge0-style-hidden");
-                }
-            });
-        });
     },
     reverse(name) {
         document.querySelectorAll(`.judge0-${name}-hidden`).forEach(e => {
-            e.classList.remove("judge0-style-hidden");
+            e.classList.remove("judge0-hidden");
         });
     }
 };
@@ -40,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (IS_ELECTRON) {
         style.apply("electron");
     } else if (IS_PUTER) {
-        style.apply("puter");
+        style.apply("standalone");
     } else {
         style.apply(query.get("style"));
     }
